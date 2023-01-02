1d2e6d3a9901b3f3c917b2b78a592aa21d3a19a5
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-01-02 16:22:36 -0500

    Developed product schema

---
+++ models/product.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const { ObjectId } = mongoose.Schema;
+
+const productSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            trim: true,
+            required: true,
+            maxLength: 32
+        },
+        description: {
+            type: String,
+            required: true,
+            maxLength: 2000
+        },
+        price: {
+            type: Number,
+            trim: true,
+            required: true,
+            maxLength: 32
+        },
+        //Using another model as each product must have a category(relationship)
+        category: {
+            type: ObjectId,
+            ref: 'Category',
+            required: true
+        },
+        quantity: {
+            type: Number,
+        },
+        photo: {
+            data: Buffer,
+            contentType: String
+        },
+        shipping: {
+            type: Boolean,
+            required: true,
+        }
+    }, { timestamps: true }
+)
+
+module.exports = mongoose.model('Product', productSchema)
