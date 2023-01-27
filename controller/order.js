a134c133d5e99cb64f17bdd1928c0610ebb2d344
patelrutwik1999 <rutwikpatel290499@gmail.com>
2023-01-27 17:50:03 -0500

    Developed two controller middleware one is orderById which is executed when there will be order id in the url and other one is updateOrderStatus which will be responsible to update the status of given order.

--- controller/order.js
+++ controller/order.js
@@ -1,5 +1,20 @@
 const { Order, CartItem } = require('../models/order')
 const { errorHandler } = require('../helpers/dbErrorHandler')
+const product = require('../models/product')
+
+exports.orderById = (req, res, next, id) => {
+    Order.findById(id)
+        .populate('products.product', 'name price')
+        .exec((err, order) => {
+            if (err || !order) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            req.order = order
+            next()
+        })
+}
 
 exports.create = (req, res) => {
     // console.log('Create Order:', req.body)
@@ -26,4 +41,12 @@ exports.listOrders = (req, res) => {
 //Send enum values to the front-end
 exports.getStatusValue = (req, res) => {
     res.json(Order.schema.path('status').enumValues)
+}
+
+exports.updateOrderStatus = (req, res) => {
+    Order.update({ _id: req.body.orderId }, { $set: { status: req.body.status } }, (err, order) => {
+        if (err) return res.status(400).json({ error: errorHandler(err) })
+
+        res.json(order)
+    })
 }
--- routes/order.js
+++ routes/order.js
@@ -4,14 +4,16 @@ const router = express.Router();
 
 const { requireSignIn, isAuth, isAdmin } = require('../controller/auth');
 const { userById, addOrderToHistory } = require('../controller/user');
-const { create, listOrders, getStatusValue } = require('../controller/order');
+const { create, listOrders, getStatusValue, orderById, updateOrderStatus } = require('../controller/order');
 const { decreaseQuantity } = require('../controller/product');
 
 router.post('/order/create/:userId', requireSignIn, isAuth, addOrderToHistory, decreaseQuantity, create)
 router.get('/order/list/:userId', requireSignIn, isAuth, isAdmin, listOrders)
-router.get('order/status-values/:userId', requireSignIn, isAdmin, isAuth, getStatusValue)
+router.get('/order/status-values/:userId', requireSignIn, isAdmin, isAuth, getStatusValue)
+router.put('/order/:orderId/status/:userId', requireSignIn, isAuth, isAdmin, updateOrderStatus)
 
 
 router.param('userId', userById)
+router.param('orderId', orderById)
 
 module.exports = router
